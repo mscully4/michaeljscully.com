f5fd50ed4ac88faf95e0ed397437ce4e07c2e9d7
Michael <michael.scully1997@gmail.com>
2020-12-14 02:47:33 -0500

    Refactored Boot Script

--- EC2-Boot-Script.sh
+++ EC2-Boot-Script.sh
@@ -1,24 +1,25 @@
-sudo su 
-apt-get update
-apt-get install nginx
-
-sudo apt update
-sudo apt install python3-pip
-
-
-#clone repo from Github
-#move build/* to /var/www/html
-#move nginx/nginx.conf to /etc/nginx/sites-available/
-#create symlink from /etc/nginx/sites-available/nginx.conf to /etc/nginx/sites-enabled/nginx.conf
-
-#Node Installation
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo apt install nodejs
-
-#Jenkins Installation
-sudo apt install openjdk-8-jdk
-
+#Jenkins Dependencies
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > \
     /etc/apt/sources.list.d/jenkins.list'
-sudo apt-get install jenkins
+
+#Node Depedencies
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+
+sudo apt update
+sudo apt-get update
+
+sudo apt-get -y install nginx
+sudo apt -y install python3-pip
+sudo apt -y install nodejs
+sudo apt -y install openjdk-8-jdk
+sudo apt-get -y install jenkins
+sudo apt install gunicorn
+sudo apt install awscli
+python3 -m pip install venv
+
+#Download Jenkins CLI
+wget 3.21.221.96:8080/jnlpJars/jenkins-cli.jar -O /home/ubuntu/
+
+#Grant Jenkins Sudo Access
+echo "jenkins ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/jenkins
