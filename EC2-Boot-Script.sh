db6df77eb9636e92dadd37bdc8d8acac321db30b
Michael <michael.scully1997@gmail.com>
2020-12-24 14:29:57 -0500

    Changes to Boot Script

--- EC2-Boot-Script.sh
+++ EC2-Boot-Script.sh
@@ -2,14 +2,13 @@
 
 curl -sL https://rpm.nodesource.com/setup_15.x | sudo bash -
 
+sudo amazon-linux-extras -y install python3.8
+sudo amazon-linux-extras -y install nginx1
+
+
 sudo yum-config-manager --enable epel
 sudo yum -y update
-sudo yum -y install python3 
-sudo yum -y install nginx
-sudo yum -y install jq
-sudo yum -y install java-1.8.0
-sudo yum -y install git
-sudo yum -y install nodejs
+sudo yum -y install jq java-1.8.0 git nodejs
 
 sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
@@ -17,6 +16,8 @@ sudo yum -y install jenkins
 
 #Grant Jenkins Sudo Access
 echo "jenkins ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/jenkins
+
+sudo service jenkins start
 
 wget https://raw.githubusercontent.com/mscully4/michaeljscully.com/master/install_jenkins_plugins.sh -O ./install_jenkins_plugins.sh
 wget https://raw.githubusercontent.com/mscully4/michaeljscully.com/master/config.xml -O ./config.xml
