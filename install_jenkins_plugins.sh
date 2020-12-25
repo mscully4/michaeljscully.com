0cf4cdfaea540ff73726057789b64f43fc48bce2
Michael <michael.scully1997@gmail.com>
2020-12-24 22:44:23 -0500

    split jenkins file in 2

--- install_jenkins_plugins.sh
+++ install_jenkins_plugins.sh
@@ -25,28 +25,6 @@ sleep 30s
 curl -X POST --user "admin:${JENKINS_TOKEN}" 'http://localhost:8080/safeRestart'
 sudo echo "$(date): Restarted Jenkins" >> /home/ubuntu/log.txt
 
-sleep 300s
-
+sleep 180s
 sudo echo "$(date): Slept for a long time" >> /home/ubuntu/log.txt
-#Need to re-authenticate after restart
-#Retrieve password
-PASSWORD=$(sudo cat /var/lib/jenkins/secrets/initialAdminPassword)
 
-#Retrieve Crumb and Token
-JENKINS_CRUMB=$(curl -sS --cookie-jar ./cookie "http://admin:${PASSWORD}@localhost:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,\":\",//crumb)")
-JENKINS_TOKEN=$(curl -sS --cookie ./cookie -H $JENKINS_CRUMB 'http://localhost:8080/me/descriptorByName/jenkins.security.ApiTokenProperty/generateNewToken' -X POST --data 'newTokenName=jenkins' --user admin:$PASSWORD | jq .data.tokenValue)
-
-#Strip extra quotes from JENKINS_TOKEN
-JENKINS_TOKEN=$(echo "${JENKINS_TOKEN//\"/}")
-sudo echo "$(date): Reauthenticated Jenkins" >> /home/ubuntu/log.txt
-
-#Create the job
-curl -XPOST 'http://localhost:8080/createItem?name=michaeljscully.com' -u "admin:${JENKINS_TOKEN}" --data-binary @config.xml -H "Content-Type:text/xml"
-sudo echo "$(date): Submitted Job to Jenkins" >> /home/ubuntu/log.txt
-
-
-sleep 5s
-
-#Build the job
-curl -XPOST 'http://localhost:8080/job/michaeljscully.com/build' -u "admin:${JENKINS_TOKEN}"
-sudo echo "$(date): Built the job" >> /home/ubuntu/log.txt
