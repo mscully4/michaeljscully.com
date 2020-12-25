993520e4f392e2fc58dd640708fb9b7bde9aa1c5
Michael <michael.scully1997@gmail.com>
2020-12-25 15:55:38 -0500

    add sudo

--- user-data.sh
+++ user-data.sh
@@ -3,9 +3,9 @@
 touch log.txt
 chown ubuntu:ubuntu /home/ubuntu/log.txt
 
-echo "$(date): Begin" >> log.txt
+sudo echo "$(date): Begin" >> log.txt
 wget https://raw.githubusercontent.com/mscully4/michaeljscully.com/master/EC2-Boot-Script.sh -O /home/ubuntu/EC2-Boot-Script.sh 
 chmod +x /home/ubuntu/EC2-Boot-Script.sh 
 
-echo "$(date): Running Boot Script" >> log.txt
+sudo echo "$(date): Running Boot Script" >> log.txt
 /home/ubuntu/EC2-Boot-Script.sh
