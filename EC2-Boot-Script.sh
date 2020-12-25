faed2dc532158a0fdafa88c6bcb89c9e45da40b7
Michael <michael.scully1997@gmail.com>
2020-12-25 13:03:01 -0500

    Add logging file

--- EC2-Boot-Script.sh
+++ EC2-Boot-Script.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+#Create logging file
+touch /home/ubuntu/log.txt
+chown ubuntu:ubuntu /home/ubuntu/log.txt
+
 #Jenkins Dependencies
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > \
