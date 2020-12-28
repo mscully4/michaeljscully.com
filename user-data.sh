0fcd451c12139c95673d84cb4131944e0eaf66a5
Michael <michael.scully1997@gmail.com>
2020-12-27 23:01:53 -0500

    change relative paths to absolute in user data

--- user-data.sh
+++ user-data.sh
@@ -1,11 +1,11 @@
 #!/bin/bash 
 
-touch log.txt
-chown ubuntu:ubuntu /home/ubuntu/log.txt
+touch /home/ubuntu/log.txt
+sudo chown ubuntu:ubuntu /home/ubuntu/log.txt
 
-sudo echo "$(date): Begin" >> log.txt
+sudo echo "$(date): Begin" >> /home/ubuntu/log.txt
 wget https://raw.githubusercontent.com/mscully4/michaeljscully.com/master/EC2-Boot-Script.sh -O /home/ubuntu/EC2-Boot-Script.sh 
 chmod +x /home/ubuntu/EC2-Boot-Script.sh 
 
-sudo echo "$(date): Running Boot Script" >> log.txt
+sudo echo "$(date): Running Boot Script" >> /home/ubuntu/log.txt
 /home/ubuntu/EC2-Boot-Script.sh
