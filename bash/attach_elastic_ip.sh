8dbb3e4f70aa944362f68692f05e692308995e46
Michael <michael.scully1997@gmail.com>
2021-03-19 22:14:01 -0400

    Turn allocate_elastic_ip into a function

--- bash/attach_elastic_ip.sh
+++ bash/attach_elastic_ip.sh
@@ -1,11 +1,16 @@
-#! /bin/bash
-
 REGION="us-east-2"
+ELASTIC_IP="3.21.221.96"
 INSTANCE_ID=$(wget -q -O - http://169.254.169.254/latest/meta-data/instance-id)
-ADDRESSES=$(aws ec2 describe-addresses --region $REGION)
 
-if (( $(jq ".Addresses | length" <<< "$ADDRESSES") > 1 ))
-    then echo "More than 1 Elastic IP"
-fi
+function allocate_elastic_ip() {
+        ADDRESSES=$(aws ec2 describe-addresses --region $3 | jq -r ".Addresses[].PublicIp")
 
-aws ec2 associate-address --instance-id $INSTANCE_ID --region us-east-2 --public-ip 3.21.221.96
+        if [[ $ADDRESSES == *$1* ]]; then
+                aws ec2 associate-address --instance-id $2 --region $3 --public-ip $ELASTIC_IP
+                return 1
+        else
+                return 0
+        fi
+}
+
+allocate_elastic_ip $ELASTIC_IP $INSTANCE_ID $REGION
