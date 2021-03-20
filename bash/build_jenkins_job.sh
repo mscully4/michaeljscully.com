f3bae2123c27603247b2fbc9f922cd73cf873c75
Michael <michael.scully1997@gmail.com>
2021-03-19 23:13:30 -0400

    Convert Build Jenkins job into function

--- bash/build_jenkins_job.sh
+++ bash/build_jenkins_job.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-sudo echo "$(date): Jenkins HTTP Response Code $(curl -s -o /dev/null -w "%{http_code}" localhost:8080/login)" >> /home/ubuntu/log.txt
-
 #Retrieve password
 PASSWORD=$(sudo cat /var/lib/jenkins/secrets/initialAdminPassword)
+JENKINS_CRUMB=$(curl -sS --cookie-jar ./cookie "http://admin:${3}@localhost:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,\":\",//crumb)")
 
-#Retrieve Crumb and Token
-JENKINS_CRUMB=$(curl -sS --cookie-jar ./cookie "http://admin:${PASSWORD}@localhost:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,\":\",//crumb)")
-JENKINS_TOKEN=$(curl -sS --cookie ./cookie -H $JENKINS_CRUMB 'http://localhost:8080/me/descriptorByName/jenkins.security.ApiTokenProperty/generateNewToken' -X POST --data 'newTokenName=jenkins' --user admin:$PASSWORD | jq .data.tokenValue)
+JENKINS_TOKEN=$(curl -sS --cookie ./cookie -H $JENKINS_CRUMB "${1}/me/descriptorByName/jenkins.security.ApiTokenProperty/generateNewToken" -X POST --data 'newTokenName=jenkins' --user admin:$PASSWORD | jq .data.tokenValue | tr -d '\"' )
+JENKINS_URL='http://localhost:8080'
+JOB_NAME='michaeljscully.com'
+LOGGING_PATH=/home/ubuntu/log.txt
 
-#Strip extra quotes from JENKINS_TOKEN
-JENKINS_TOKEN=$(echo "${JENKINS_TOKEN//\"/}")
-sudo echo "$(date): Retrieved Jenkins Tokens" >> /home/ubuntu/log.txt
+function build_jenkins_job() {
+        sudo echo "$(date): Retrieved Jenkins Tokens" >> $4
+        
+        curl -vs -XPOST "$1/createItem?name=$2" -u "admin:${3}" --data-binary @/home/ubuntu/config.xml -H "Content-Type:text/xml" >> $4 2>&1
+        sudo echo "$(date): Submitted Job to Jenkins" >> $4
+        
+        sleep 5s
 
-#Create the job
-curl -vs -XPOST 'http://localhost:8080/createItem?name=michaeljscully.com' -u "admin:${JENKINS_TOKEN}" --data-binary @/home/ubuntu/config.xml -H "Content-Type:text/xml" >>/home/ubuntu/create.txt 2>&1
-sudo echo "$(date): Submitted Job to Jenkins" >> /home/ubuntu/log.txt
+        curl -XPOST "$1/job/$2/build" -u "admin:${JENKINS_TOKEN}"
+        sudo echo "$(date): Built the job" >> $4
+}
 
-sleep 5s
-
-#Build the job
-curl -XPOST 'http://localhost:8080/job/michaeljscully.com/build' -u "admin:${JENKINS_TOKEN}"
-sudo echo "$(date): Built the job" >> /home/ubuntu/log.txt
+build_jenkins_job $JENKINS_URL $JOB_NAME $JENKINS_TOKEN $LOGGING_PATH
