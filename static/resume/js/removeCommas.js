5f7ebea2c4ebf098809ed321060031f8c6ebad59
Michael <michael.scully1997@gmail.com>
2021-03-21 20:37:42 -0400

    Deleted unused files

--- static/resume/js/removeCommas.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(() => {
-  $(".removeCommas").each(function(i, el) {
-    el.textContent = el.textContent.length > 1 ? el.textContent.slice(1) : el.textContent
-  })
-    //.slice(1))
-  // $(".removeCommas").text($(".removeCommas").text().substr(1))
-})
