2a3aa5892dc531ee024e8068ff0dc2bd4e874488
Michael <michael.scully1997@gmail.com>
2021-04-10 00:38:16 -0400

    Convert to apex charts

--- investing/src/Returns.js
+++ investing/src/Returns.js
@@ -5,11 +5,14 @@ import { dateFormatter, percentageFormatter, dollarFormatter } from './utils.js'
 import { interpolateRdYlGn } from 'd3-scale-chromatic'
 import clsx from 'clsx';
 import { withStyles } from '@material-ui/core/styles';
+import Chart from "react-apexcharts";
+
 
 const styles = {
   graphContainer: {
     position: 'absolute',
-    left: "60%",
+    top: "60%",
+    left: "7.5%",
   },
   toolTip: {
     padding: "5px 10px",
@@ -26,81 +29,107 @@ class Returns extends React.Component {
     }
   }
 
-  CustomTooltip = ({ active, payload, label }) => {
-    if (active && payload && payload.length) {
-      const data = payload[0].payload;
-      if (this.props.measure === MEASURE_PERCENT) {
-        return (
-          <div style={styles.toolTip}>
-            <p className="label">{`Date: ${dateFormatter(label)}`}</p>
-            <p className="desc">{`Percent Gain: ${percentageFormatter(data.percentage_gain)}`}</p>
-            <p className="desc">{`Net: ${dollarFormatter(data.net, 2)}`}</p>
-
-          </div>
-        )
-      } else if (this.props.measure === MEASURE_DOLLARS) {
-        return (
-          <div style={styles.toolTip}>
-            <p className="label">{`Date: ${dateFormatter(label)}`}</p>
-            <p className="desc">{`Total Value: ${dollarFormatter(data.total_value, 2)}`}</p>
-            <p className="desc">{`Total Cost: ${dollarFormatter(data.total_cost, 2)}`}</p>
-            <p className="desc">{`Net: ${dollarFormatter(data.net, 2)}`}</p>
-          </div>
-        );
-      }
-    }
-
-    return null;
-  };
-
-  legendFormatter = (value, entry, index) => {
-    return value.split('_').map((s) => s.charAt(0).toUpperCase() + s.substring(1)).join(' ');
-  }
-
   render = () => {
     const classes = this.props.classes;
-    if (this.props.measure === MEASURE_PERCENT) {
+    const measure = this.props.measure;
+
+    let chartSeries = [{
+      name: 'Total Return',
+    }];
+
+    let chartOptions = {
+      chart: {
+        type: 'area',
+        stacked: false,
+        height: 350,
+        zoom: {
+          type: 'x',
+          enabled: true,
+          autoScaleYaxis: true
+        },
+        toolbar: {
+          autoSelected: 'zoom'
+        }
+      },
+      dataLabels: {
+        enabled: false
+      },
+      markers: {
+        size: 0,
+      },
+      title: {
+        text: null,
+        align: 'left'
+      },
+      fill: {
+        type: 'gradient',
+        gradient: {
+          shadeIntensity: 1,
+          inverseColors: false,
+          opacityFrom: 0.5,
+          opacityTo: 0,
+          stops: [0, 90, 100]
+        },
+      },
+      yaxis: {
+        labels: {
+          formatter: null
+        },
+        title: {
+          text: null
+        },
+      },
+      xaxis: {
+        type: 'datetime',
+      },
+      tooltip: {
+        shared: true,
+        y: {
+          formatter: null
+        }
+      },
+      colors: ["#000"]
+    }
+
+    if (measure === MEASURE_PERCENT) {
+      const data = this.props.data.map((value) => {
+        return [value.date, value.percentage_gain]
+      })
+
+      chartSeries[0].data = data;
+
+      chartOptions.title.text = "Total Return in Percent"
+      chartOptions.tooltip.y.formatter = (x) => percentageFormatter(x, 2)
+      chartOptions.yaxis.labels.formatter = (x) => percentageFormatter(x, 0)
+      chartOptions.yaxis.title = "Percent"
+
       return (
-        <ResponsiveContainer height={"30%"} width={"30%"} className={clsx(classes.graphContainer)}>
-          <LineChart data={this.props.data}>
-            <XAxis
-              dataKey='date'
-              domain={["auto", "auto"]}
-              name='Date'
-              tickFormatter={dateFormatter}
-            />
-            <YAxis domain={[0, 'auto']} tickFormatter={percentageFormatter} />
-            <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
-            <Tooltip content={this.CustomTooltip} />
-            <Legend formatter={this.legendFormatter} />
-            <Line type="monotone" dataKey="percentage_gain" stroke={"#000"} />
-          </LineChart>
+        <ResponsiveContainer height={"30%"} width={"40%"} className={clsx(classes.graphContainer)}>
+          <Chart options={chartOptions} series={chartSeries} type="area" />
         </ResponsiveContainer>
       )
     }
-    else if (this.props.measure === MEASURE_DOLLARS) {
+    else if (measure === MEASURE_DOLLARS) {
+      const data = this.props.data.map((value) => {
+        return [value.date, value.total_value - value.total_cost]
+      })
+
+      chartSeries[0].data = data;
+
+      chartOptions.title.text = "Total Return in Dollars"
+      chartOptions.tooltip.y.formatter = (x) => dollarFormatter(x, 2)
+      chartOptions.yaxis.labels.formatter = (x) => dollarFormatter(x, 2)
+      chartOptions.yaxis.title = "Dollars"
+
       return (
-        <ResponsiveContainer height={"30%"} width={"30%"} className={clsx(classes.graphContainer)}>
-          <LineChart data={this.props.data}>
-            <XAxis
-              dataKey='date'
-              domain={["auto", "auto"]}
-              name='Date'
-              tickFormatter={dateFormatter}
-            />
-            <YAxis domain={[0, 'auto']} tickFormatter={(t) => dollarFormatter(t, 0)} />
-            <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
-            <Tooltip content={this.CustomTooltip} />
-            <Legend formatter={this.legendFormatter} />
-            <Line type="monotone" dataKey="total_value" stroke={"#000"} />
-            <Line type="monotone" dataKey="total_cost" stroke={interpolateRdYlGn(0)} />
-          </LineChart>
+        <ResponsiveContainer height={"30%"} width={"40%"} className={clsx(classes.graphContainer)}>
+          <Chart options={chartOptions} series={chartSeries} type="area" />
         </ResponsiveContainer>
 
       )
     }
     else {
-      return <div>asdfs</div>
+      return (<div></div>)
     }
   }
 }
