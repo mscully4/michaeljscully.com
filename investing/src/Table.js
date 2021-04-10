8b28c780e498ecbe4250576bbc54f9de99dc9fe4
Michael <michael.scully1997@gmail.com>
2021-04-10 00:38:02 -0400

    Change table styling

--- investing/src/Table.js
+++ investing/src/Table.js
@@ -7,16 +7,21 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import { withStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
 
 import { dollarFormatter } from './utils.js'
 
 const styles = {
-  accountInfoContainer: {
+  tableContainer: {
     position: 'absolute',
-    top: '50%',
-    left: '60%',
-    width: '30%'
+    top: '20%',
+    left: '27.5%',
+    width: '20%',
   },
+  row: {
+    paddingTop: '5%',
+    paddingBottom: '5%'
+  }
 };
 
 class DataTable extends React.Component {
@@ -30,34 +35,33 @@ class DataTable extends React.Component {
   render = () => {
     const classes = this.props.classes;
     const account = this.props.account;
-    console.log(this.props)
     const transfers = this.props.transfers.reduce((acc, cur) => {
       return acc + cur.net_amount
     }, 0)
 
     return (
-      <TableContainer component={Paper} className={classes.accountInfoContainer}>
+      <TableContainer component={Paper} className={classes.tableContainer}>
         <Table className={classes.table} size='small'>
           <TableBody>
             <TableRow key={"cash"}>
-              <TableCell component="th" scope="row">Cash</TableCell>
-              <TableCell align="right">{dollarFormatter(account.cash, 2)}</TableCell>
+              <TableCell className={clsx(classes.row)} component="th" scope="row">Cash</TableCell>
+              <TableCell align="right">+{dollarFormatter(account.cash, 2)}</TableCell>
             </TableRow>
             <TableRow key={"equity"}>
-              <TableCell component="th" scope="row">Equity Value</TableCell>
-              <TableCell align="right">{dollarFormatter(account.long_market_value, 2)}</TableCell>
+              <TableCell className={clsx(classes.row)} component="th" scope="row">Equity Value</TableCell>
+              <TableCell align="right">+{dollarFormatter(account.long_market_value, 2)}</TableCell>
             </TableRow>
-            <TableRow key={"total"}>
-              <TableCell component="th" scope="row">Total Value</TableCell>
-              <TableCell align="right">{dollarFormatter(account.cash + account.long_market_value, 2)}</TableCell>
+            <TableRow key={"total"} >
+              <TableCell className={clsx(classes.row)} component="th" scope="row">Total Value</TableCell>
+              <TableCell align="right" style={{ borderTop: "solid 2px black" }}>={dollarFormatter(account.cash + account.long_market_value, 2)}</TableCell>
             </TableRow>
             <TableRow key={"invested"}>
-              <TableCell component="th" scope="row">Amount Invested</TableCell>
-              <TableCell align="right">{dollarFormatter(transfers, 2)}</TableCell>
+              <TableCell className={clsx(classes.row)} component="th" scope="row">Amount Invested</TableCell>
+              <TableCell align="right">-{dollarFormatter(transfers, 2)}</TableCell>
             </TableRow>
             <TableRow key={"net"}>
-              <TableCell component="th" scope="row">Net</TableCell>
-              <TableCell align="right">{dollarFormatter(account.cash + account.long_market_value - transfers, 2)}</TableCell>
+              <TableCell className={clsx(classes.row)} component="th" scope="row">Net Gain/Loss</TableCell>
+              <TableCell align="right" style={{ borderTop: "solid 2px black" }}>={dollarFormatter(account.cash + account.long_market_value - transfers, 2)}</TableCell>
             </TableRow>
           </TableBody>
         </Table>
