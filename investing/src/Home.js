faf64e61ab3e70e2605049c19a6fa1e8aef20934
Michael <michael.scully1997@gmail.com>
2021-04-17 01:53:38 -0400

    Cleaned some things up

--- investing/src/Home.js
+++ investing/src/Home.js
@@ -11,7 +11,6 @@ import { Switch, Grid } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 import clsx from 'clsx';
 
-// import Grid from '@material-ui/core/Grid';
 
 import { MEASURE_DOLLARS, MEASURE_PERCENT, BASE_URL } from './constants.js'
 
@@ -19,14 +18,17 @@ const measureEnum = [MEASURE_DOLLARS, MEASURE_PERCENT]
 
 const styles = {
   title: {
-    fontSize: 50,
-    textAlign: 'center'
+    fontSize: '6vw',
+    textAlign: 'center',
+    fontFamily: 'aguafina-script',
+    marginTop: '5vh',
+    marginBottom: 0
   },
   //Grid
   gridContainer: {
     display: 'grid',
     gridTemplateRows: '1fr 1fr',
-    gridTemplateColumns: '1fr 1fr 1fr 1fr',
+    gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr 1fr',
     height: 750,
     width: "90%",
     marginLeft: '7.5%',
@@ -57,28 +59,30 @@ const styles = {
     width: "100%",
     height: "100%",
     gridRow: "1 / 3",
-    gridColumn: "3 / 5"
+    gridColumn: "4 / 7"
   },
   //Returns Graph
   lineGraphContainer: {
     width: "90%",
     height: "90%",
     gridRow: "2",
-    gridColumn: '1/3'
+    gridColumn: '1/4'
   },
   //Data Table
   dataTable: {
     width: "90%",
     margin: 'auto',
+    marginTop: 0,
     gridRow: "1",
-    gridColumn: '2'
+    gridColumn: '3'
   },
   //Description
   description: {
+    textIndent: 10,
     width: "90%",
     margin: '0 auto',
     gridRow: "1",
-    gridColumn: "1"
+    gridColumn: "1 / 3"
   }
 };
 
@@ -126,7 +130,6 @@ class Home extends React.Component {
     const classes = this.props.classes;
     return (
       <div className={clsx(classes.treeMapContainer)}>
-        {/* <span>Positions</span> */}
         {this.props.positions ? (<TreeMap holdings={this.props.positions} measure={measure} />) : null}
       </div>
     )
@@ -147,6 +150,13 @@ class Home extends React.Component {
     )
   }
 
+  renderDescription = () => {
+    return (
+    <p className={clsx(this.props.classes.description)}>
+      This is my algorithmic trading portfolio!  I've hand picked around 50 tech(ish) stocks that I believe will build the future!  I created a Lambda Function to run every trading day, rebalancing my portfolio according to my weighting algorithm, which is the cube root of each company's market cap. By using the cube root, I decreased the weight of larger companies and increased the weight of smaller ones.  When the function runs every day, it calculates what my portfolio should look like and compares that with what it actually looks like.  Ideally, it would buy and sell to meet this desired allocation, but by selling, I would be realizing capital gains and therefore would have to pay taxes.  So I changed my algorithm to never sell and to buy in such a way as to get as close to the desired allocation as possible.  I have also added functionality to transfer money into my account at random intervals, to take advantage of dollar cost averaging.  To the bottom and to the right, you can see how I am doing, and all the way to the right you can see what companies I own!
+    </p>)
+  }
+
   render = () => {
     const classes = this.props.classes;
     const measure = measureEnum[this.state.measure];
@@ -158,7 +168,7 @@ class Home extends React.Component {
         {this.renderSwitch(measure)}
 
         <div className={classes.gridContainer}>
-          <p className={clsx(classes.description)}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed enim nulla. Sed eget viverra urna. Maecenas convallis turpis sed odio varius laoreet. Phasellus libero tellus, venenatis et convallis eu, venenatis sed leo. Praesent et euismod mauris, at tempus turpis. Sed tincidunt felis quis eros.</p>
+          {this.renderDescription()}
           {this.renderDataTable(classes.dataTable)}
           {this.renderReturnsGraph(measure, classes.lineGraphContainer)}
           {this.renderTreeMap(measure)}
