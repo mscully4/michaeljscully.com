0531b64f03fd007e0bfe7f47d1ce0b0572a54a5b
Michael <michael.scully1997@gmail.com>
2021-07-07 22:48:53 -0400

    Add map constants to constants file

--- travel/src/utils/Constants.js
+++ travel/src/utils/Constants.js
@@ -3,3 +3,8 @@ export const API_DESTINATIONS = `${API_BASE}/destinations`
 export const API_PLACES = `${API_BASE}/places`
 export const API_PHOTOS = `${API_BASE}/photos`
 export const API_ALBUMS = `${API_BASE}/albums`
+
+//Mapping Constants
+export const GOOGLE_MAPS_API_KEY = 'AIzaSyAk_bN5yfkLuUzptVXIHWs59YdFmI_TjAc'
+export const DEFAULT_CENTER = { lat: 33.7490, lng: -84.3880 }
+export const GRANULARITY_CUTOFF = 8
